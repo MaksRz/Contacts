2ae3721d651d604c80282b7b789004f233a2b95f
MaksRz <1901183@uad.ac.uk>
2021-10-05 15:43:10 +0100

    Added person.cs file in person folder

---
+++ Person/person.cs
@@ -0,0 +1,11 @@
+namespace Contacts.Person
+{
+    public class Person
+    {
+        public string Forname { get; set; }
+        public string Surname { get; set; }
+        public double Age { get; set; }
+        public string SSN { get; set; }
+
+    }
+}
