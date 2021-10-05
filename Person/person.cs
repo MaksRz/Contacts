21c0a71857db637fcf690acfc3e5f3dfa7acb14e
MaksRz <91138801+MaksRz@users.noreply.github.com>
2021-10-05 16:19:46 +0100

    Update person.cs
    Added comment to test pull functionality in VS

--- Person/person.cs
+++ Person/person.cs
@@ -6,6 +6,6 @@ namespace Contacts.Person
         public string Surname { get; set; }
         public double Age { get; set; }
         public string SSN { get; set; }
-
+        //data aded in github.com
     }
 }
